6934238c11927cda5f5a4a109179661abb65241f
fengyi <fengyi.ling@exxonmobile.com>
2022-06-30 22:26:25 +0800

    [Fengyi] Explain what lambda can do and introduce functional interface

---
+++ src/main/java/com/fengyi/javastream/interface_demo/README.md
@@ -0,0 +1,2 @@
+lambda can only replace the interface that just have one abstract method
+also, this kind of interface is called functional interface
--- src/main/java/com/fengyi/javastream/interface_demo/creature/LayEggCreature.java
+++ src/main/java/com/fengyi/javastream/interface_demo/creature/LayEggCreature.java
@@ -2,4 +2,6 @@ package com.fengyi.javastream.interface_demo.creature;
 
 public interface LayEggCreature {
   String layEgg();
+  // not allow here
+  //  void walk();
 }
